81019402615e61732c0d4bb59786ef65b636d3da
jtirkkon <jouni.tirkkonen@gmail.com>
2021-03-06 16:43:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState }  from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import './App.css';
 
 function App() {
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [todos, setTodos] = useState([]);
+  
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, {description: description, date: date}]);
+  }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  } 
+
+  const handleDateChange = (date) => {
+    setDate(date);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <DatePicker value={date} onChange={date => handleDateChange(date)}/>
+      </MuiPickersUtilsProvider>
+      <TextField name="description" label="Description" onChange={handleDescriptionChange} value={description} 
+      style={{marginLeft: 50, marginRight: 50}}/>
+      <Button onClick={addTodo} variant="contained" color="primary">Add</Button>
+      <table><tbody>
+      {
+      todos.map((todo, index) => 
+        <tr key={index}>
+          <td>{todo.date.toString().slice(0, 15)}</td>
+          <td>{todo.description}</td>
+        </tr>)
+      }
+      </tbody></table>
     </div>
   );
 }
